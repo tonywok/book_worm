2c5e7340cf2cd153ec7e3e2e6f7a2772b338dd8b
Tony Schneider <tonywok@gmail.com>
2010-08-22 21:01:46 -0400

    removed support for dewey decimal and icc_number

--- lib/book_worm/book.rb
+++ lib/book_worm/book.rb
@@ -3,27 +3,32 @@ require 'cgi'
 module BookWorm
   class Book < Searchable
     
-    INDICES = %w[isbn title combined full]
+    INDICES = [:isbn, :title, :combined, :full,
+               :book_id, :person_id, :publisher_id]
 
     attr_accessor :publisher, :authors, :isbn,
                   :isbn13, :title, :long_title, 
                   :book_id
 
     def initialize(book_data)
-      self.long_title = book_data["TitleLong"]
-      self.publisher  = book_data["PublisherText"]
-      self.authors    = book_data["AuthorsText"]
-      self.isbn       = book_data["isbn"]
-      self.isbn13     = book_data["isbn13"]
-      self.title      = book_data["Title"]
-      self.book_id    = book_data["book_id"]
+      self.long_title    = book_data["TitleLong"]
+      self.publisher     = book_data["PublisherText"]
+      self.authors       = book_data["AuthorsText"]
+      self.isbn          = book_data["isbn"]
+      self.isbn13        = book_data["isbn13"]
+      self.title         = book_data["Title"]
+      self.book_id       = book_data["book_id"]
     end
 
     class << self
 
       INDICES.each do |index|
-        define_method "search_by_#{index}" do |arg|
-          search(index, arg)
+        define_method "find_book_by_#{index}" do |arg|
+          find_book(index, arg)
+        end
+
+        define_method "find_books_by_#{index}" do |arg|
+          find_books(index, arg)
         end
       end
 
@@ -33,15 +38,24 @@ module BookWorm
       #   combined - search across titles, authors, and publisher name
       #   full     - search across itles, authors, publisher name, summary, 
       #              notes, awards information, etc
-      def search(index, value)
+      def find_books(index, value)
         normalize(query_isbndb(index, value))
+      end
+      
+      def find_book(index, value)
+        normalize(query_isbndb(index, value))[0]
       end
 
       private
 
       def normalize(results)
-        books = [results['ISBNdb']['BookList']['BookData']].flatten
-        books.size == 1 ? Book.new(books[0]) : books.collect {|book| Book.new(book) }
+        begin 
+          [results['ISBNdb']['BookList']['BookData']].flatten.collect do |book| 
+            Book.new(book)
+          end
+        rescue
+          []
+        end
       end
 
       def query_isbndb(index, value)
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,24 +2,63 @@ require 'spec_helper'
 require 'lib/book_worm'
 
 describe BookWorm do
-  context "When querying for a book." do
+  context "When querying for a single book," do
+    context "and the index is explicitly known," do 
 
-    it "should be able to search explicitly by isbn" do
-      ruby_book = BookWorm::Book.search(:isbn, "1934356085")
-      ruby_book.isbn.should == "1934356085"
+      it "should find by isbn." do
+        result = BookWorm::Book.find_book(:isbn, "1934356085")
+        
+        result.isbn.should == "1934356085"
+      end
+
+      it "should find by title." do
+        result = BookWorm::Book.find_book(:title, "Programming Ruby 1.9")
+        result.title.should == "Programming Ruby 1.9"
+      end
+
+      it "should find by titles, authors, and publisher name" do
+        result = BookWorm::Book.find_book(:combined, "Ruby Thomas Pragmatic")
+        result.title.should =~ /ruby|rails/i
+      end
+
+      it "should find by practically all information ISBNdb.com has about books." do
+        result = BookWorm::Book.find_book(:full, "pragmatic ruby rails thomas")
+        result.title.should =~ /ruby|rails/i
+      end
     end
 
-    it "should be able to 'search by isbn'" do
-      ruby_book = BookWorm::Book.search_by_isbn("1934356085")
-      ruby_book.isbn.should == "1934356085"
-    end
+    #   it "should search with the publisher_id index" do
+    #     ruby_book = BookWorm::Book.search(:publisher_id, "978-1934356081")
+    #     ruby_book.isbn.should == "978-1934356081"
+    #   end
 
-    it "should be able to search by title" do
-      results = BookWorm::Book.search(:title, 'Ruby')
-      results.each do |book|
-        return true if book.title == "Programming Ruby 1.9"
-      end.should be_true
-    end
+    #   it "should search with the subject_id index" do
+    #     ruby_book = BookWorm::Book.search(:subject_id, "978-1934356081")
+    #     ruby_book.isbn.should == "978-1934356081"
+    #   end
+
+    #   it "should search with the icc_number index" do
+    #     ruby_book = BookWorm::Book.search(:icc_number, "978-1934356081")
+    #     ruby_book.isbn.should == "978-1934356081"
+    #   end
+
+    #   it "should search with the dewey_decimal index" do
+    #     ruby_book = BookWorm::Book.search(:dewey_decimal, "978-1934356081")
+    #     ruby_book.isbn.should == "978-1934356081"
+    #   end
+    # end
+
+    # it "should be able to 'search by isbn'" do
+    #   ruby_book = BookWorm::Book.search_by_isbn("1934356085")
+    #   ruby_book.isbn.should == "1934356085"
+    # end
+
+    # it "should be able to search by title" do
+    #   results = BookWorm::Book.search(:title, 'Ruby')
+    #   results.each do |book|
+    #     return true if book.title == "Programming Ruby 1.9"
+    #   end.should be_true
+    # end
   end
 end
 
